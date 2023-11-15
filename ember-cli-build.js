95323ec8715f1da5cbc00d0b5787ac722e893334
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2023-11-15 16:57:51 +0300

    Attempt to reproduce the issue

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,10 +5,19 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    '@embroider/macros': {
+      setConfig: {
+        '@ember-data/store': {
+          polyfillUUID: true,
+        },
+      },
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
     skipBabel: [
       {
         package: 'qunit',
